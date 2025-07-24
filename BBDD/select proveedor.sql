0e3d74c3523014ac57bb28074ef9b25e9087d9d2
Sole1999 <1ccontapatsyrios@gmail.com>
2025-07-24 16:47:18 -0500

    Reto 9 Crear Proveedor, subido el proyecto exitosamente.

--- BBDD/select proveedor.sql
+++ BBDD/select proveedor.sql
@@ -1,3 +1,14 @@
 select identificador, tipo_de_documento_fk, nombre, telefono, correo, direccion
 from proveedores
 where upper(nombre) like '%SA%'
+
+
+select identificador, tipo_de_documento_fk, td.descripcion, nombre, telefono, correo, direccion
+from proveedores prov, tipo_documentos td
+where prov.tipo_de_documento_fk = td.codigo
+and upper(nombre) like '%SA%'
+
+select * from tipo_documentos
+
+insert into tipo_documentos(codigo, descripcion)
+values('P', 'PASAP')
